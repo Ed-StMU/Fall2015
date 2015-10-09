6915e0c5e0ccea68a06dfbc92f67c980864e6518
Edmonk <Edmonk@10.102.123.46>
2015-10-09 10:43:52 -0500

    ColorPanels Challenge

--- GUIChallenge/src/ColorPanels.java
+++ GUIChallenge/src/ColorPanels.java
@@ -21,46 +21,40 @@ public class ColorPanels extends JFrame {
 		JPanel p2 = new JPanel();
 		p2.setSize(80, 100);
 		p2.setLocation(150, 50);
-		p2.setBackground(Color.GREEN);
+		p2.setBackground(Color.BLUE);
 
 		JPanel p3 = new JPanel();
 		p3.setSize(80, 100);
 		p3.setLocation(275, 50);
 		p3.setBackground(Color.YELLOW);
 
-		JLabel r = new JLabel("Red");
-		r.setSize(25, 25);
-		r.setLocation(55, 25);
+		JLabel rLabel = new JLabel("Red");
+		rLabel.setBounds(55, 25, 25, 25);
 
-		JLabel g = new JLabel("Green");
-		g.setSize(50, 25);
-		g.setLocation(174, 25);
+		JLabel bLabel = new JLabel("Blue");
+		bLabel.setBounds(175, 25, 50, 25);
 
-		JLabel y = new JLabel("Yellow");
-		y.setSize(50, 25);
-		y.setLocation(296, 25);
+		JLabel yLabel = new JLabel("Yellow");
+		yLabel.setBounds(296, 25, 50, 25);
 
 		JButton rButton = new JButton("Red");
-		rButton.setSize(60, 25);
-		rButton.setLocation(36, 160);
-		
-		JButton gButton = new JButton("Green");
-		gButton.setSize(70, 25);
-		gButton.setLocation(155, 160);
-		
+		rButton.setBounds(36, 160, 60, 25);
+
+		JButton bButton = new JButton("Blue");
+		bButton.setBounds(155, 160, 70, 25);
+
 		JButton yButton = new JButton("Yellow");
-		yButton.setSize(72, 25);
-		yButton.setLocation(280, 160);
-		
+		yButton.setBounds(280, 160, 72, 25);
+
 		add(main);
 		main.add(p1);
 		main.add(p2);
 		main.add(p3);
-		main.add(r);
-		main.add(g);
-		main.add(y);
+		main.add(rLabel);
+		main.add(bLabel);
+		main.add(yLabel);
 		main.add(rButton);
-		main.add(gButton);
+		main.add(bButton);
 		main.add(yButton);
 
 		setVisible(true);
