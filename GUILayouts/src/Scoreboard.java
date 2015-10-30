93d972d5b4867eb0486774896f367f63b19b85be
Edmonk <Edmonk@10.102.224.108>
2015-10-30 15:48:14 -0500

    Challenge

--- GUILayouts/src/Scoreboard.java
+++ GUILayouts/src/Scoreboard.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Scoreboard extends JFrame {
+
+	private int redScore = 0;
+	private int blueScore = 0;
+	private JLabel redScoreLabel = new JLabel("0");
+	private JLabel blueScoreLabel = new JLabel("0");
 
 	public Scoreboard() {
 
@@ -24,30 +30,61 @@ public class Scoreboard extends JFrame {
 		JLabel label2 = new JLabel("Blue Team");
 		label2.setForeground(Color.BLUE);
 
-		JLabel label3 = new JLabel("0");
-		JLabel label4 = new JLabel("0");
-
 		JButton redButton = new JButton("Red Score!");
 		JButton blueButton = new JButton("Blue Score!");
 		JButton reset = new JButton("Reset Score");
+		redButton.addActionListener(new AddRedScore());
+		blueButton.addActionListener(new AddBlueScore());
+		reset.addActionListener(new ResetScore());
 
 		panel1.add(label1);
 		panel2.add(label2);
-		panel3.add(label3);
-		panel4.add(label4);
+		panel3.add(redScoreLabel);
+		panel4.add(blueScoreLabel);
 		panel5.add(redButton);
 		panel6.add(blueButton);
+
 		gridPanel.add(panel1);
 		gridPanel.add(panel2);
 		gridPanel.add(panel3);
 		gridPanel.add(panel4);
 		gridPanel.add(panel5);
 		gridPanel.add(panel6);
+
 		add(gridPanel, BorderLayout.CENTER);
 		add(reset, BorderLayout.SOUTH);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+
+	public class AddRedScore implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+
+			redScore++;
+			redScoreLabel.setText(Integer.toString(redScore));
+		}
+	}
+
+	public class AddBlueScore implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+
+			blueScore++;
+			blueScoreLabel.setText(Integer.toString(blueScore));
+		}
+	}
+
+	public class ResetScore implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+
+			redScore = 0;
+			blueScore = 0;
+			redScoreLabel.setText(Integer.toString(redScore));
+			blueScoreLabel.setText(Integer.toString(blueScore));
+		}
 	}
 
 	public static void main(String[] args) {
